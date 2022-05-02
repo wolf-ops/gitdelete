8216f0ac1132c3c7694042e702a361fc2cce9673
shenhui <shenhui@136.com>
2022-05-02 09:28:57 +0800

    hot first commit

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
         System.out.println("hello,git!");
         System.out.println("git.hello");
         System.out.println("git-beautiful");
+        System.out.println("你好，宗乐文");
     }
 
 }
